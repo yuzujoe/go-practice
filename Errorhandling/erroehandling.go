0c8d4525433a707b8c14db81fad7b0895ca61d62
yuzujoe <yuzun225@gmail.com>
2019-03-06 12:03:49 +0900

    erroehandling

---
+++ Errorhandling/erroehandling.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./erroehandling.go")
+	if err != nil {
+		log.Fatal("Error")
+	}
+
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
+
+	if err = os.Chdir("test"); err != nil {
+		log.Fatalln("destoroy")
+	}
+
+}
