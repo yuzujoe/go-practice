c08f3b816b86616e37647de4affa096074da1982
yuzujoe <yuzun225@gmail.com>
2019-03-06 17:21:43 +0900

    Embedded

--- Struct/struct.go
+++ Struct/struct.go
@@ -22,15 +22,37 @@ func (v *Vertex) Scale(i int) {
 // 	return v.X * v.Y
 // }
 
-// 外部のパーケージから呼び出すときにNEWを使う時が多い
-func New(x, y int) *Vertex {
-	return &Vertex{x, y}
+// // 外部のパーケージから呼び出すときにNEWを使う時が多い
+// func New(x, y int) *Vertex {
+// 	return &Vertex{x, y}
+// }
+
+// ~Embedded~
+type Vertex3D struct {
+	Vertex
+	z int
 }
+
+func (v Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
+}
+
+// ~Embedded~
 
 func main() {
 	// v := Vertex{3, 4}
 	// fmt.Println(Area(v))
-	v := New(3, 4)
-	v.Scale(10)
-	fmt.Println(v.Area())
+	v := New(3, 4, 5)
+	v.Scale3D(10)
+	fmt.Println(v.Area3D())
 }
