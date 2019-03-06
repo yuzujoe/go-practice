6aaecc5b5e642ff2c95d1b6bd334b45a77dc95d9
yuzujoe <yuzun225@gmail.com>
2019-03-06 20:27:12 +0900

    producer consumer

--- Goroutine/goroutine.go
+++ Goroutine/goroutine.go
@@ -95,9 +95,43 @@ func channelRange() {
 
 // channel range close
 
+// producer consumer
+
+func producer(ch chan int, i int) {
+	ch <- i * 2
+}
+
+func consumer(ch chan int, wg *sync.WaitGroup) {
+	for i := range ch {
+		func() {
+			defer wg.Done()
+			fmt.Println("process", i*1000)
+		}()
+	}
+}
+
+func prpducerConsumer() {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	// producer
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go producer(ch, i)
+	}
+
+	// consumer
+	go consumer(ch, &wg)
+	wg.Wait()
+	close(ch)
+}
+
+// producer consumer
+
 func main() {
 	// helloworld()
 	// channel()
 	// bufferChannel()
-	channelRange()
+	// channelRange()
+	prpducerConsumer()
 }
