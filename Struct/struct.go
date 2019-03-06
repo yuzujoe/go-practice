59587a01205e5e904d92fe599a23a7a4474810c6
yuzujoe <yuzun225@gmail.com>
2019-03-06 16:44:39 +0900

    constract

--- Struct/struct.go
+++ Struct/struct.go
@@ -4,27 +4,33 @@ import "fmt"
 
 // this type declaration Vertex
 type Vertex struct {
-	X, Y int
+	x, y int
 }
 
 func (v Vertex) Area() int {
-	return v.X * v.Y
+	return v.x * v.y
 }
 
 // *をつけることでVertexに紐づいたScaleというメソッドを呼び出すことができる
 func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
+	v.x = v.x * i
+	v.y = v.y * i
 }
 
 // looking for coordinates with this function
-func Area(v Vertex) int {
-	return v.X * v.Y
+// func Area(v Vertex) int {
+// 	return v.X * v.Y
+// }
+
+// 外部のパーケージから呼び出すときにNEWを使う時が多い
+func New(x, y int) *Vertex {
+	return &Vertex{x, y}
 }
 
 func main() {
-	v := Vertex{3, 4}
+	// v := Vertex{3, 4}
 	// fmt.Println(Area(v))
+	v := New(3, 4)
 	v.Scale(10)
 	fmt.Println(v.Area())
 }
