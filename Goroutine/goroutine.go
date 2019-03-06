d60ac29560627cce763d12c1ea9bfbd8ae6b411c
yuzujoe <yuzun225@gmail.com>
2019-03-06 22:10:28 +0900

    fanOut fanIn

--- Goroutine/goroutine.go
+++ Goroutine/goroutine.go
@@ -128,10 +128,49 @@ func prpducerConsumer() {
 
 // producer consumer
 
+// fan out fan in
+
+func multi(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
+}
+
+func multi2(first <-chan int, second chan<- int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
+	}
+}
+
+func multi4(second <-chan int, third chan<- int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 4
+	}
+}
+
+func fanOut() {
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
+
+	go multi(first)
+	go multi2(first, second)
+	go multi4(second, third)
+	for result := range third {
+		fmt.Println(result)
+	}
+}
+
+// fan out fan in
+
 func main() {
 	// helloworld()
 	// channel()
 	// bufferChannel()
 	// channelRange()
-	prpducerConsumer()
+	// prpducerConsumer()
+	fanOut()
 }
