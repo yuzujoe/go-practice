53a1f1f4e16e6ad6f2d2a4955eff0795bca0d6e5
yuzujoe <yuzun225@gmail.com>
2019-03-06 12:52:46 +0900

    演習

--- Practice/practice.go
+++ Practice/practice.go
@@ -15,7 +15,42 @@ func maping() {
 	fmt.Printf("%T %v\n", m, m)
 }
 
+func searchmin() {
+	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+
+	var min int
+	for i, num := range l {
+		if i == 0 {
+			min = num
+			continue
+		} else if min >= num {
+			min = num
+		}
+		fmt.Println(min)
+	}
+}
+
+func addFruits() {
+	m := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+
+	sum := 0
+	for _, v := range m {
+		sum += v
+	}
+
+	fmt.Println(sum)
+}
+
 func main() {
 	convert()
 	maping()
+	searchmin()
+	addFruits()
 }
