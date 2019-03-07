fb04f2c8ae8eb31cd3f6ee2fff5cb9f6e2a80259
yuzujoe <yuzun225@gmail.com>
2019-03-07 14:26:15 +0900

    test

---
+++ Package/mylib/math_test.go
@@ -0,0 +1,15 @@
+package mylib
+
+import "testing"
+
+var Debug = true
+
+func TestAverage(t *testing.T) {
+	if Debug {
+		t.Skip("Skip Reason")
+	}
+	v := Average([]int{1, 2, 3, 4, 5, 6, 7})
+	if v != 3 {
+		t.Error("Expected 3,got", v)
+	}
+}
