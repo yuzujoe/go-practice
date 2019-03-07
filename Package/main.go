71d917ca0983b5fa4225e32159119ec820f2dabf
yuzujoe <yuzun225@gmail.com>
2019-03-07 13:40:12 +0900

    Public privete

--- Package/main.go
+++ Package/main.go
@@ -13,4 +13,9 @@ func main() {
 	fmt.Println(mylib.Average(s))
 	mylib.Say()
 	under.Hello()
+
+	person := mylib.Person{Name: "Mike", Age: 20}
+	fmt.Println(person)
+
+	fmt.Println(mylib.Public)
 }
--- Package/mylib/human.go
+++ Package/mylib/human.go
@@ -2,6 +2,18 @@ package mylib
 
 import "fmt"
 
+// Public privete
+
+var Public string = "Public"
+
+// 頭文字を小文字にするとprivateになって出力できない
+type Person struct {
+	Name string
+	Age  int
+}
+
+// Public privete
+
 func Say() {
 	fmt.Println("Human!")
 }
